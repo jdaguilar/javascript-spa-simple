9fdb140bdec31de3612d0442486de9cb8718c1ab
JorgeAguilarOrozco <jorge.aguilar@safekeepingapp.com>
2021-07-07 23:46:05 -0500

    organize imports

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import { router } from "./router/index.routes";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.scss'
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./sass/main.scss";
+import {router} from './router/index.routes'
+
 
 const init = () => {
-  router(window.location.hash);
-
-  window.addEventListener("hashchange", () => {
     router(window.location.hash);
-  });
-};
-
-window.addEventListener("load", init);
+  
+    window.addEventListener("hashchange", () => {
+      router(window.location.hash);
+    });
+  };
+  
+  window.addEventListener("load", init);
+  
